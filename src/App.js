487f19b324a29026c607647f82a588d8f9535546
Lauro Kirsch <lauro.kirsch@gmail.com>
2019-11-11 01:38:19 -0300

    added basic navigation to menu-items with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route exact path='/hats' component={HatsPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
