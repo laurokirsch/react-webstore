7275418905932d5dfa43c2768cb331227cb28600
Lauro Kirsch <lauro.kirsch@gmail.com>
2019-11-12 22:05:33 -0300

    Shop page, collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Homepage from './pages/homepage/homepage.component';
+
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Router>
         <Switch>
-          <Route exact path='/' component={Homepage} />
-          <Route exact path='/hats' component={HatsPage} />
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
         </Switch>
       </Router>
     </div>
